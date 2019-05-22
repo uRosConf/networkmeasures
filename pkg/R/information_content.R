35a816211f5995fc21858578f913e76a81d71f07
Edwin de Jonge <edwindjonge@gmail.com>
2019-05-22 10:07:33 +0200

    shuffling files around

---
+++ pkg/R/information_content.R
@@ -0,0 +1,10 @@
+#' Topological information content
+#' @param g a graph
+#' 
+#' @details
+#' The topological information content is defined as
+#' the logarithm of the size of the automorphism group to the base of 2.
+#' 
+information_content <- function(g){
+  log2(as.numeric(igraph::automorphisms(g)$group_size))
+}
